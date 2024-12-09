c56015bb567e516b98548f2a3c3bedd1969fb068
akcchi <161343155+akcchi@users.noreply.github.com>
2024-12-09 00:54:15 +0000

    Fix error due to concurrent users

--- app.py
+++ app.py
@@ -89,7 +89,8 @@ def update():
 
 app = Flask(__name__, static_url_path="/assets", static_folder="static/assets")
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tables.db"
+#app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tables.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///file::memory:?uri=true"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = os.urandom(24).hex()
 
